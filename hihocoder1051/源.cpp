68b28f60b18fe0c18d98b99a6038a9b9e7e0e837
appvin <appvin@outlook.com>
2017-03-20 15:01:16 +0800

    ac

--- hihocoder1051/源.cpp
+++ hihocoder1051/源.cpp
@@ -12,12 +12,25 @@ int main()
 		int M, N;
 		cin >> N >> M;
 		vector<int> v;
-		v.resize(N);
-		for (int in = 0; in < N; in++)
+		v.resize(N + 2);
+		for (int in = 1; in < N + 1; in++)
 		{
 			cin >> v[in];
 		}
+		v[N + 1] = 100;
 		if (N <= M)
 			cout << "100" << endl;
+		else
+		{
+			int max = 0;
+			int sum = 0;
+			for (int ii = 0; ii <= N - M; ii++)
+			{
+				sum = v[ii + M + 1] - v[ii] - 1;
+				if (sum > max)
+					max = sum;
+			}
+			cout << max << endl;
+		}
 	}
 }
